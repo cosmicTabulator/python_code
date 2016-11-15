7c0de4a3be5dc1299552a4e26f118ac24932f7f7
gmc1221 <grahamcooke1221@gmail.com>
2016-11-15 16:14:03 -0500

    Bugfixes + Search() and chackSpace()
    Lots of bugfixes, rebuilding search() and checkSpace()

--- SudokuDancingLinks.py
+++ SudokuDancingLinks.py
@@ -5,20 +5,47 @@ Created on Tue Oct 25 11:33:57 2016
 @author: Graham Cooke
 """
 
-#PS Future self remember to comment (;
+#PS Future self: remember to comment (;
 
 import random
-import sys
-import copy
+import logging
+logger = logging.getLogger()
 
 def row(k):
-    return (mult(k[1],indexes) - k)
+    m = mult(indexes,k[1])
+    out = []
+    for e in m:
+        if e != k:
+            out.append(e)
+    return out
 
 def col(k):
-    return (mult(k[0],indexes) - k)
+    m = mult(k[0],indexes)
+    out = []
+    for e in m:
+        if e != k:
+            out.append(e)
+    return out
 
 def cel(k):
-    return (checkCell(k) - k)
+    m = checkCell(k)
+    out = []
+    for e in m:
+        if e != k:
+            out.append(e)
+    return out
+    
+def fRow(k):
+    return mult(indexes,k)
+
+def fCol(k):
+    return mult(k,indexes)
+
+def fCel(k):
+    y = str((int(k) // 3)*3)
+    x = str((int(k) % 3)*3)
+    k = x+y
+    return (checkCell(k))
 
 def mult(list1,list2):
     out = []
@@ -28,32 +55,33 @@ def mult(list1,list2):
     return out
     
 def checkSpace(n,k):
-        if n in (grid[i] for i in related[k]):
-            return False
-        else:
-            return True
-    
+    neighbors = []
+    for i in related[k]:
+        if i not in empty:
+            neighbors.append(grid[i])
+    if n in neighbors:
+        return False
+    else:
+        return True
+
 def checkCell(k):
-    cellPoints = []
-    xC = ((k[0]-1) // 3)
-    yC = ((k[1]-1) // 3)
-
-    if(xC is 0):
-        xC = indexC1
-    elif(xC is 1):
-        xC = indexC2
-    else:
-        xC = indexC3
-    if(yC is 0):
-        yC = indexC1
-    elif(yC is 1):
-        yC = indexC2
-    else:
-        yC = indexC3
-
-    cellPoints.append(mult(xC,yC))
-
-    return (cellPoints)
+      xC = (int(k[0]) // 3)
+      yC = (int(k[1]) // 3)
+  
+      if(xC is 0):
+          xC = indexC1
+      elif(xC is 1):
+          xC = indexC2
+      else:
+          xC = indexC3
+      if(yC is 0):
+          yC = indexC1
+      elif(yC is 1):
+          yC = indexC2
+      else:
+          yC = indexC3
+  
+      return (mult(xC,yC))
 
 def poss():
     for k in open:
@@ -62,109 +90,62 @@ def poss():
                 grid[k] = grid[k] + n
 
 def search():
-    for k in open:
+    for k in empty:
         if len(grid[k]) is 1:
             reduce(grid[k],k)
-            open.pop(open.index(k))
+            empty.pop(empty.index(k))
+        for n in grid[k]:
+            isolated = True
+            for r in row(k):
+                if n in grid[r]:
+                    isolated = False
+            if isolated:
+                grid[k] = n
+                reduce(n,k)
+                break
+            isolated = True
+            for c in col(k):
+                if n in grid[c]:
+                    isolated = False
+            if isolated:
+                grid[k] = n
+                reduce(n,k)
+                break
+            isolated = True
+            for c in cel(k):
+                if n in grid[c]:
+                    isolated = False
+            if isolated:
+                grid[k] = n
+                empty.pop(empty.index(k))
+                reduce(n,k)
+                break
 
 def reduce(n,k):
     for s in related[k]:
         grid[s] = grid[s].replace(n,"")
-
-# def generate(x,y):
-#
-#     l = [1,2,3,4,5,6,7,8,9]
-#     c = findCell(x,y)
-#     for n in range(9):
-#         if(n+1 in column[x] or n+1 in row[y] or n+1 in cell[c]):
-#             i = l.index(n+1)
-#             l.pop(i)
-#     if(l != []):
-#         grid[y][x] = random.choice(l)
-#     else:
-#         print("error on {},{}; no possible number".format(x,y))
-#         for i in range(9):
-#             print(row[i])
-#         sys.exit()
-#     return
-    
-# def possible(x,y):
-#     l = [1,2,3,4,5,6,7,8,9]
-#     c = findCell(x,y)
-#     for n in range(9):
-#         if(n+1 in column[x] or n+1 in row[y] or n+1 in cell[c]):
-#             i = l.index(n+1)
-#             l.pop(i)
-#     return len(l)
-
-# def getUnsolved():
-#     unsolved = []
-#     for x in range(9):
-#         for y in range(9):
-#             if solvedGrid[y][x] == 0:
-#                 unsolved.append([x,y])
-#     return unsolved
-
-# def solve():
-#     global solvedGrid
-#     solved = []
-#     solvedGrid = copy.deepcopy(grid)
-#     unsolved = getUnsolved()
-#     updateSolved()
-#     cycles = 0
-#     while(unsolved):
-#         cycles += 1
-#         x = unsolved[0][0]
-#         y = unsolved[0][1]
-#         c = findCell(x,y)
-#         n = solvedGrid[y][x]
-#         n += 1
-#         while((n in sColumn[x] or n in sRow[y] or n in sCell[c]) and n <= 9):
-#             n += 1
-#         if(n > 9):
-#             try:
-#                 wipe = solved[-1]
-#             except:
-#                 return False
-#             unsolved.insert(0, wipe)
-#             del solved[-1]
-#             solvedGrid[y][x] = 0
-#         else:
-#             solvedGrid[y][x] = n
-#             del unsolved[0]
-#             solved.append([x,y])
-#         updateSolved()
-#     print(cycles)
-#     return True
-                
-
+        
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
 indexes = "012345678"
 indexC1 = "012"
 indexC2 = "345"
 indexC3 = "678"
 numbers = "123456789"
-all = mult(indexes,indexes)
+full = mult(indexes,indexes)
 
 #Define the sudoku grid
 grid = {k : 0 for k in mult(indexes,indexes)}
+grid.update({"r"+k : fRow(k) for k in indexes})
+grid.update({"c"+k : fCol(k) for k in indexes})
+grid.update({"s"+k : fCel(k) for k in indexes})
 related = {k : (row(k) + col(k) + cel(k)) for k in mult(indexes,indexes)}
-
-open = []
-
-
-
-# while(len(generated) < givens):
-#     loc = random.choice(spaces)
-#     x = loc[0]
-#     y = loc[1]
-#     print(x,y)
-#     if(possible(x,y) > 0):
-#         generate(x,y)
-#         if(solve()):
-#             generated.append(loc)
-#             i = spaces.index(loc)
-#             spaces.pop(i)
-#             print(len(generated))
-#         else:
-#             grid[y][x] = 0
-#             print("tick")
+logging.debug(related)
+logging.debug(grid)
+empty = []
+for n in indexes:
+    numbers = ()
+    for i in grid["r"+n]:
+        numbers = numbers + (i,)
+    logger.debug(numbers)
+    logger.info("{0} {1} {2} | {3} {4} {5} | {6} {7} {8}".format(numbers))
