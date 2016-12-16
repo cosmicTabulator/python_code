28b95163e029e48a96270a00781a988e98c59e6d
gmc1221 <grahamcooke1221@gmail.com>
2016-12-15 21:59:04 -0500

    Tried working on Display
    Display shows up now, but not uniform width.
    Consider switching to the .grid() geometry manager

--- sudoku/gui.py
+++ sudoku/gui.py
@@ -9,6 +9,8 @@ import tkinter
 import solver
 import logging
 
+#Rewrite using .grid()
+
 logger = logging.getLogger("Gui")
 logger.setLevel(logging.DEBUG)
 
@@ -21,12 +23,12 @@ def make():
     b = tkinter.Button(root,text = "Create")
     b["command"] = getValue
     b.pack(side = "bottom")
-    
+
 def getValue():
     value = 0
     try:
         value = int(e.get())
-        if( value < 30 or value > 80):
+        if value < 30 or value > 80:
             t["text"] = "Please enter a number between 30 and 81"
         else:
             unsolved = solver.unsolve(value)
@@ -34,25 +36,15 @@ def getValue():
             display(unsolved)
     except:
         t["text"] = "Please Enter a number"
-    return   
+    return
 
-def ask():
-    value = 0
-    while value < 30 or value > 80:
-        t["text"] = "Please enter a value between 30 and 81"
-        try:
-            value = int(e.get())
-        except:
-            t["text"] = "Please enter a number"
-    return value
-    
 def display(u):
-    d = []
     line = 0
     l = 0
     logger.debug("Entered")
     logger.debug(u)
-    print(" ------- ------- -------")
+    d[l]["text"] = " ------- ------- -------"
+    l += 1
     for x in solver.indexes:
         logger.debug("Line " + str(x))
         numbers = ()
@@ -60,12 +52,19 @@ def display(u):
             numbers = numbers + (u[x+y],)
             logger.debug("Y " + str(y))
         logger.debug(numbers)
-        d[l]["text"] = ("| {0:1} {1:1} {2:1} | {3:1} {4:1} {5:1} | {6:1} {7:1} {8:1} |".format(*numbers))
+        for x in range(len(numbers)):
+            if numbers[x] == "":
+                numbers[x] == "_"
+        logger.debug(numbers)
+        text = "| {0:1} {1:1} {2:1} | {3:1} {4:1} {5:1} | {6:1} {7:1} {8:1} |".format(*numbers)
+        logger.debug("Built")
+        d[l]["text"] = text
         logger.debug("Printed Line " + str(x))
         line += 1
         l += 1
-        if ((line % 3) == 0 and line != 9):
+        if (line % 3) == 0 and line != 9:
             d[l]["text"] = " -------+-------+-------"
+            l += 1
     logger.debug("Printed?")
     d[l]["text"] = " ------- ------- -------"
     for l in range(len(d)):
@@ -94,9 +93,9 @@ e.pack()
 q = tkinter.Button(root, text = "Quit", command = root.destroy)
 q.pack()
 
-d = [tkinter.Label(root, text = "|") for x in range(13)]
+d = [tkinter.Label(root, text = "|", width = 25) for x in range(13)]
 for l in range(len(d)):
-    d[l]["text"] = "{}".format("1")
+    d[l]["text"] = "| 1 2 3 | 4 5 6 | 7 8 9 |"
     d[l].pack()
     logger.debug(d[l])
 
